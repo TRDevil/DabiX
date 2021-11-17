d77bbdb2745c9021c977372588e6afd315d649bb
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-11-17 20:25:24 +0530

    Update chatbots.py

--- mrjoker/modules/chatbots.py
+++ mrjoker/modules/chatbots.py
@@ -1,12 +1,12 @@
 __help__= f"""
-🤡 *Mr.Joker Luna AI Chatbot* 
+*Nezuko Luna AI Chatbot* 
 
  🔹 `/chatbot` on/off: Enables and disables Luna AI Chat mode (EXCLUSIVE)
  🔹 `/chatbot en `: Enables English only chatbot
  
 *Powered by* : [Luna ChatBot](https://github.com/TheHamkerCat/LunaChatBot)
 
-🤡 *Mr.Joker Kuki AI Chatbot* 
+*Nezuko Kuki AI Chatbot* 
 
 🔹 `/addkuki `: Enables and Disables Kuki AI Chat mode (EXCLUSIVE)
 
