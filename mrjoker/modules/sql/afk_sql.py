213f3a8a75284ecbd33d9fc33fd32dc623dc4ee6
DC Owner <91002607+DCOwnerNo1@users.noreply.github.com>
2021-12-10 09:59:35 +0530

    Update afk_sql.py

--- mrjoker/modules/sql/afk_sql.py
+++ mrjoker/modules/sql/afk_sql.py
@@ -1,13 +1,12 @@
 import threading
 
 from mrjoker.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, UnicodeText
-
+from sqlalchemy.sql.sqltypes import BigInteger
 
 class AFK(BASE):
     __tablename__ = "afk_users"
 
-    user_id = Column(Integer, primary_key=True)
+    user_id from Integer to BigInteger
     is_afk = Column(Boolean)
     reason = Column(UnicodeText)
 
