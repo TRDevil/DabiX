3a81aecf9bbf84ee29743e1809ee8c3b19b338ac
TR Crazy ✠ <91002607+TRDevil@users.noreply.github.com>
2022-01-15 16:40:40 +0530

    `ᴄʜᴀᴛʙᴏᴛs`

--- mrjoker/modules/chatbots.py
+++ mrjoker/modules/chatbots.py
@@ -1,16 +1,16 @@
 __help__= f"""
-*Nezuko Luna AI Chatbot* 
+*SelenX AI Chatbot* 
 
  🔹 `/chatbot` on/off: Enables and disables Luna AI Chat mode (EXCLUSIVE)
  🔹 `/chatbot en `: Enables English only chatbot
  
 *Powered by* : [Luna ChatBot](https://github.com/TheHamkerCat/LunaChatBot)
 
-*Nezuko Kuki AI Chatbot* 
+*SelenX AI Chatbot* 
 
-🔹 `/addkuki `: Enables and Disables Kuki AI Chat mode (EXCLUSIVE)
+🔹 `/selenxai `: Enables and Disables Kuki AI Chat mode (EXCLUSIVE)
 
 *Powered by* : [Kuki Chatbot](https://github.com/MoeZilla/KukiChatbot)
 """
 
-__mod_name__ = "ChatBots"
+__mod_name__ = "ᴄʜᴀᴛʙᴏᴛs"
